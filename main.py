bf2d363720e490c18946fbe5f63a75d5220a7c24
Pavan Khairnar <85490004+pavansk00@users.noreply.github.com>
2022-01-31 10:32:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-from flask import Flask,jsonify,request
-from day3_3 import player_request
+from flask import Flask,request
 
-app = Flask(_name_)
+app=Flask(__name__)
 
-@app.route('/playerrequest',methods=["POST"])
-def player():
-    request_data=request.get_json()
-    player1_name=request_data['player1']
-    player1_symbol=request_data['symbol1']
-    player2_name=request_data['player2']
-    player2_symbol=request_data['symbol2']
-    
-    winner=player_request(player1_name,player1_symbol,player2_name,player2_symbol)
-    
-    
+@app.route("/p1_input",methods=["POST"])
+def p1_input ():
+    if request =="POST":
+        input= request.form["text"]
+        process_text=input.upper()
+        return  process_text
 
-    if winner == player1_symbol:
-        return f"Players_Names = {player1_name, player2_name} \n" \
-               f"Players_Symbol = {player1_symbol, player2_symbol} \n" \
-               f"The winner of the game 'TIC-Tac-Toa' is '{player1_name}' "
-    if winner == player2_symbol:
-        return f"Players_Names = {player1_name, player2_name} \n" \
-               f"Players_Symbol = {player1_symbol, player2_symbol} \n" \
-               f"The winner of the game 'TIC-Tac-Toa' is '{player2_name}' "
+@app.route("/p2_input",methods=["POST"])
+def p2_input ():
+    if request =="POST":
+        input= request.form["text"]
+        process_text=input.upper()
+        return  process_text
 
-
-if _name_ == "_main_": 
-    
-   app.run(host="0.0.0.0", port=3000, debug=True
+if __name__=="__main__":
+    if player == 1:
+        p1_input()
+    else:
+        p2_input()
+    app.run()
